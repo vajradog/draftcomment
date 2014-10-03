a8d9d042caa6bd63c24a0a8fd7c028740a5e1d3c
vajradog <vajradog@gmail.com>
2014-10-03 16:50:48 -0400

    cleaned up controller

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-== README
-
 Created this for a friend to demonstrate a simple way to set comments as 'draft' by default and then moderate them to 'approve'. Only 'approved' comments will appear to the site visitors.
 
 This quick demo is completely bare-bone rails app (except for Zurb Foundation) and has no authentication, validation etc. built in (which you would probably want for a real site). The purpose is just to demonstrate the simple method of moderating comments. No moderation gems, just plain logic.
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    #@comment.user_id = current_user.id 
     @comment.status = "draft"
     if @comment.save
       redirect_to post_path(@post), notice: "Thank you. Your comment was created and is awaiting moderation. Once approved it will appear on this page momentarily."
@@ -21,52 +20,9 @@ class CommentsController < ApplicationController
     redirect_to post_comments_path
   end
 
-  
-
-  #def edit
-  #  @comment = Comment.find(params[:id])
-  #  @commentable = @comment.commentable
-
-  #end
-
-  #def update
-  #  @comment = Comment.find(params[:id])
-  #  @commentable = @comment.commentable
-
-
-  #  if @comment.update(comment_params)
-  #    flash[:success] = "comment updated"
-  #    redirect_to root_path
-  #  else
-  #    flash[:error] = "Could not update comment"
-   #   render 'edit'
-  #  end
-  #end
-
-  #def destroy
-  #@comment = Comment.find(params[:id])
-  #@commentable = @comment.commentable
-  #if @comment.destroy
-  #  flash[:success] = "Comment Destroyed!"   
-  #  redirect_to :back
-  #end
-#end
-
-#def approve
-    #Comment.update_all({status: "approved"}, {id: params[:comment_ids]})
-    #redirect_to statement_comments_path
-  #end
-
-
-
   private
 
   def comment_params
     params.require(:comment).permit(:body, :post_id, :creator, :status)
   end
-
-  #def load_commentable
-   # klass = [Statement].detect { |c| params["#{c.name.underscore}_id"]}
-   # @commentable = klass.find(params["#{klass.name.underscore}_id"])
-  #end
 end
